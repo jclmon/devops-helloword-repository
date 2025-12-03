51dd838cdd2c5b07c39a2b059b9b6da98ac0e1b2
jclmon <37410371+jclmon@users.noreply.github.com>
2025-12-03 17:17:40 +0100

    Update api_test.py
    multiplicacion y division

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -25,6 +25,26 @@ class TestApi(unittest.TestCase):
             response.read().decode(), "3", "ERROR ADD"
         )
 
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/mul/1/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "2", "ERROR MULTIPLY"
+        )
+
+    def test_api_division(self):
+        url = f"{BASE_URL}/calc/div/1/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "0.5", "ERROR MULTIPLY"
+        )
+    
     def test_api_sqrt(self):
         url = f"{BASE_URL_MOCK}/calc/sqrt/64"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
