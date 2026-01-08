4f93d0754b2aae23a22c35c7b98f5084d8b9e6a5
jclmon <jclmon@outlook.es>
2026-01-08 11:42:46 +0100

    Tests unitarios de origen

--- test/unit/util_test.py
+++ test/unit/util_test.py
@@ -23,28 +23,5 @@ class TestUtil(unittest.TestCase):
         self.assertRaises(TypeError, util.convert_to_number, None)
         self.assertRaises(TypeError, util.convert_to_number, object())
 
-    def test_invalid_convert_to_number_correct_param(self):
-        self.assertEqual(4, util.InvalidConvertToNumber("4"))
-        self.assertEqual(0, util.InvalidConvertToNumber("0"))
-        self.assertEqual(0, util.InvalidConvertToNumber("-0"))
-        self.assertEqual(-1, util.InvalidConvertToNumber("-1"))
-        self.assertAlmostEqual(4.0, util.InvalidConvertToNumber("4.0"), delta=0.0000001)
-        self.assertAlmostEqual(0.0, util.InvalidConvertToNumber("0.0"), delta=0.0000001)
-        self.assertAlmostEqual(0.0, util.InvalidConvertToNumber("-0.0"), delta=0.0000001)
-        self.assertAlmostEqual(-1.0, util.InvalidConvertToNumber("-1.0"), delta=0.0000001)
-
-    def test_invalid_convert_to_number_invalid_type(self):
-        self.assertRaises(TypeError, util.InvalidConvertToNumber, "")
-        self.assertRaises(TypeError, util.InvalidConvertToNumber, "3.h")
-        self.assertRaises(TypeError, util.InvalidConvertToNumber, "s")
-        self.assertRaises(TypeError, util.InvalidConvertToNumber, None)
-        self.assertRaises(TypeError, util.InvalidConvertToNumber, object())
-
-    def test_validate_permissions_correct_param(self):
-        self.assertTrue(util.validate_permissions("login", "user1"))
-
-    def test_validate_permissions_invalid_param(self):
-        self.assertFalse(util.validate_permissions("login", "userwrong"))
-
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
